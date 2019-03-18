6a3c8f080326cfafe9d8cca3f49bbf9b85649727
Egmara <45947678+Egmara@users.noreply.github.com>
2019-03-18 00:26:17 -0300

    Add files via upload

--- AugLagModel.jl
+++ AugLagModel.jl
@@ -1,5 +1,4 @@
-#import NLPModels
-using LinearAlgebra
+using NLPModels, LinearAlgebra, LinearOperators
 
 """Given a model
 	min f(x)  s.t.  c(x) = 0, l ≦ x ≦ u,
@@ -10,6 +9,7 @@ where y is an estimates of the Lagrange multiplier vector and μ is the penalty 
 
 mutable struct AugLagModel <: AbstractNLPModel
 	meta :: NLPModelMeta
+	counters :: Counters
 	model :: AbstractNLPModel
 	y :: AbstractVector
 	mu :: Real
@@ -24,36 +24,51 @@ function AugLagModel(model :: AbstractNLPModel, y :: AbstractVector, mu :: Real)
 
 	meta = NLPModelMeta(model.meta.nvar, x0 = x0, ncon = ncon, lvar = lvar, uvar = uvar)
 
-	return AugLagModel(meta, model, y, mu)
+	return AugLagModel(meta, Counters(), model, y, mu)
 end
 
-#for counter in fieldnames(Counters)
-#	@eval begin
-#		NLPModels.$counter(nlp :: AugLagModel) = $counter(nlp.model)
-#		export $counter
-#	end
-#end
-
-#NLPModels.neval_obj(nlp :: AugLagModel) = neval_obj(nlp.model)
-
 function NLPModels.obj(nlp :: AugLagModel, x :: AbstractVector)
+	#increment!(nlp, :neval_obj)
+	nlp.counters.neval_obj += 1
 	cx = cons(nlp.model, x)
 	return obj(nlp.model, x) - dot(nlp.y, cx) + (nlp.mu / 2) * dot(cx,cx)
 end
 
 function NLPModels.grad(nlp :: AugLagModel, x :: AbstractVector)
+	nlp.counters.neval_grad += 1
 	cx = cons(nlp.model, x)
 	return grad(nlp.model, x) - jtprod(nlp.model, x, nlp.y) + nlp.mu * jtprod(nlp.model, x, cx)
 end
 
 function NLPModels.hess(nlp :: AugLagModel, x :: AbstractVector; obj_weight :: Float64 = 1.0)
+	nlp.counters.neval_hess += 1
 	cx = cons(nlp.model, x)
 	Jx = jac(nlp.model, x)
 	return obj_weight * (hess(nlp.model, x, obj_weight = obj_weight, y = nlp.mu * cx - nlp.y) + nlp.mu * tril(Jx' * Jx))
 end
 
 function NLPModels.hprod(nlp :: AugLagModel, x :: AbstractVector, v :: AbstractVector; obj_weight :: Float64 = 1.0)
+	nlp.counters.neval_hprod += 1
 	cx = cons(nlp.model, x)
 	Jv = jprod(nlp.model, x, v) # J(x)v
 	return hprod(nlp.model, x, v, obj_weight = obj_weight, y = nlp.mu * cx - nlp.y) + nlp.mu * jtprod(nlp.model, x, Jv)
+end
+
+function NLPModels.hess_op(nlp :: AugLagModel, x :: AbstractVector; obj_weight :: Float64 = 1.0)
+	return LinearOperator(Float64, nlp.meta.nvar, nlp.meta.nvar, true, true,
+			v -> NLPModels.hprod(nlp, x, v; obj_weight = obj_weight))
+end
+
+function NLPModels.hprod!(nlp :: AugLagModel, x :: AbstractVector, v :: AbstractVector, Hv :: AbstractVector;
+	obj_weight :: Float64 = 1.0)
+	# test
+	cx = cons(nlp.model, x)
+	Jv = jprod(nlp.model, x, v)
+	Hv .= hprod(nlp.model, x, v, obj_weight = obj_weight, y = nlp.mu * cx - nlp.y) + nlp.mu * jtprod(nlp.model, x, Jv)
+end
+
+function NLPModels.hess_op!(nlp :: AugLagModel, x :: AbstractVector, Hv :: AbstractVector;
+	obj_weight :: Float64 = 1.0)
+	return LinearOperator(Float64, nlp.meta.nvar, nlp.meta.nvar, true, true,
+			v -> NLPModels.hprod!(nlp, x, v, Hv; obj_weight = obj_weight))
 end
--- test.jl
+++ test.jl
@@ -1,5 +1,5 @@
-using NLPModels, LinearAlgebra, Krylov
-using Ipopt, NLPModelsIpopt
+using NLPModels, LinearAlgebra, LinearOperators
+using Krylov
 using JSOSolvers
 
 include("AugLagModel.jl")
@@ -8,7 +8,9 @@ include("al.jl")
 nlp = ADNLPModel(x->(x[1] - 1)^2 + 100*(x[2] - x[1]^2)^2, [-1.2; 1.0],
                 lvar = [0.0; 0.0], uvar = [1.0; 1.0], c = x->[x[1] + x[2] - 1], lcon = [0.0], ucon = [0.0])
 x, fx, normgL, normc, k = al(nlp)
+println("x = $x, fx = $fx, normgL = $normgL, normc = $normc, k = $k")
 
 nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2),
                  c=x->[sum(x) - 1.0], lcon = [0.0], ucon = [0.0])
 x, fx, normgL, normc, k = al(nlp)
+println("x = $x, fx = $fx, normgL = $normgL, normc = $normc, k = $k")
