c9f46ec41d783c6562dbf90c1a4569aa5784eab5
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-03-20 20:49:01 -0300

    Add TODOs and logging

--- al.jl
+++ al.jl
@@ -1,10 +1,12 @@
+using Logging, SolverTools
+
 """Implementation of a augmented Lagrangian method for:
 
 	min f(x)  s.t.  c(x) = 0, l ≦ x ≦ u"""
 
 function al(nlp :: AbstractNLPModel)
 
-	x = nlp.meta.x0
+  x = copy(nlp.meta.x0)
 	c(x) = cons(nlp, x)
 	g(x) = grad(nlp, x)
 	J(x) = jac(nlp, x)
@@ -23,13 +25,23 @@ function al(nlp :: AbstractNLPModel)
 	normcx = norm(cx)
 	iter = 0
 
-	while (normgL > 1e-5 || normcx > 1e-5) && (iter < 1000)
+  @info log_header([:iter, :normgL, :normcx], [Int, Float64, Float64])
+  @info log_row(Any[iter, normgL, normcx])
 
+  # TODO: Add keyword arguments atol, rtol, max_eval, max_iter
+  solved = normgL ≤ 1e-5 && normcx ≤ 1e-5
+  tired = iter ≥ 1000
+
+  while !(solved || tired)
+
+    # TODO: Reuse AugLagModel
 		# create subproblem
 		al_nlp = AugLagModel(nlp, y, μ)
 
 		# solve subproblem
-		S = tron(al_nlp, x = x)
+    S = with_logger(NullLogger()) do
+      tron(al_nlp, x = x)
+    end
 		x = S.solution
 
 		cx = c(x)
@@ -38,17 +50,25 @@ function al(nlp :: AbstractNLPModel)
 		normcx = norm(cx)
 
 		if normcx <= eta
-			#y = y - μ * cx
-			y = cgls(Jx', gx)[1]
+			y = y - μ * cx
+			#y = cgls(Jx', gx)[1]
 			eta = eta / μ^0.9
 		else
 			μ = 100 * μ
 			eta = 1 / μ^0.1
 		end
 
-		normgL = norm(gx - Jx' * y)
+    # TODO: Improve dual measure
+    gL = gx - Jx' * y
+    #project_step!(gpL, ...)
+    normgL = norm(gL)
 		iter += 1
+    solved = normgL ≤ 1e-5 && normcx ≤ 1e-5
+    tired = iter ≥ 1000
+
+    @info log_row(Any[iter, normgL, normcx])
 	end
 
+  # TODO: Use GenericExecutionStats
 	return x, obj(nlp,x), normgL, normcx, iter
 end
--- test.jl
+++ test.jl
@@ -14,3 +14,10 @@ nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2),
                  c=x->[sum(x) - 1.0], lcon = [0.0], ucon = [0.0])
 x, fx, normgL, normc, k = al(nlp)
 println("x = $x, fx = $fx, normgL = $normgL, normc = $normc, k = $k")
+
+nlp = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 3)^2, zeros(2),
+                 c=x->[sum(x) - 1.0], lcon = [0.0], ucon = [0.0],
+                 lvar=[0.0; 0.0], uvar=[1.0; 1.0]
+                )
+x, fx, normgL, normc, k = al(nlp)
+println("x = $x, fx = $fx, normgL = $normgL, normc = $normc, k = $k")
