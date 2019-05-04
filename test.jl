dfa102a5b8f84c2adef3aa39e81361def9a52f13
Egmara <egmara.antunes@gmail.com>
2019-05-03 23:04:05 -0300

    using markdown_table function

--- test.jl
+++ test.jl
@@ -7,8 +7,16 @@ include("al.jl")
 
 using SolverTools
 using CUTEst
+using NLPModelsIpopt
 
 pnames = CUTEst.select(max_var=2, max_con=2, only_equ_con=true)
-problems = (CUTEstModel(p) for p in pnames)
+problems = (CUTEstModel(p) for p in pnames[1:5])
 
-stats = solve_problems(al, problems) # ta na pasta bmark
+#stats = solve_problems(al, [nlp])
+
+solvers = Dict(:AugLag => al, :ipopt => ipopt)
+stats = bmark_solvers(solvers, problems)
+
+using SolverBenchmark
+markdown_table(stdout,stats[:ipopt],cols = [:name,:status,:objective,:elapsed_time])
+markdown_table(stdout,stats[:AugLag],cols = [:name,:status,:objective,:elapsed_time])
