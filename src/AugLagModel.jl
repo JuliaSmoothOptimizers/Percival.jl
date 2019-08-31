5c49ec7fce0a88d6340e55b6dfe9279bb62a0cd0
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-08-31 18:22:25 -0300

    Change tabs to spaces

--- src/AugLagModel.jl
+++ src/AugLagModel.jl
@@ -4,82 +4,82 @@ using NLPModels, LinearAlgebra, LinearOperators
 using NLPModels: increment!
 
 """Given a model
-	min f(x)  s.t.  c(x) = 0, l ≦ x ≦ u,
+  min f(x)  s.t.  c(x) = 0, l ≦ x ≦ u,
 this new model represents the subproblem of the augmented Lagrangian method
-	min f(x) - y'*c(x) + μ/2*‖c(x)‖^2  s.t.  l ≦ x ≦ u,
+  min f(x) - y'*c(x) + μ/2*‖c(x)‖^2  s.t.  l ≦ x ≦ u,
 where y is an estimates of the Lagrange multiplier vector and μ is the penalty parameter.
 """
 
 mutable struct AugLagModel <: AbstractNLPModel
-	meta :: NLPModelMeta
-	counters :: Counters
-	model :: AbstractNLPModel
-	y :: AbstractVector
-	mu :: Real
+  meta :: NLPModelMeta
+  counters :: Counters
+  model :: AbstractNLPModel
+  y :: AbstractVector
+  mu :: Real
 end
 
 function AugLagModel(model :: AbstractNLPModel, y :: AbstractVector, mu :: Real)
 
-	x0 = model.meta.x0
-	ncon = 0
-	lvar = model.meta.lvar
-	uvar = model.meta.uvar
+  x0 = model.meta.x0
+  ncon = 0
+  lvar = model.meta.lvar
+  uvar = model.meta.uvar
     nnzh = model.meta.nnzh
 
-	meta = NLPModelMeta(model.meta.nvar, x0 = x0, ncon = ncon, lvar = lvar, uvar = uvar)
+  meta = NLPModelMeta(model.meta.nvar, x0 = x0, ncon = ncon, lvar = lvar, uvar = uvar)
 
-	return AugLagModel(meta, Counters(), model, y, mu)
+  return AugLagModel(meta, Counters(), model, y, mu)
 end
 
 function NLPModels.obj(nlp :: AugLagModel, x :: AbstractVector)
-	increment!(nlp, :neval_obj)
-	cx = cons(nlp.model, x)
-	return obj(nlp.model, x) - dot(nlp.y, cx) + (nlp.mu / 2) * dot(cx,cx)
+  increment!(nlp, :neval_obj)
+  cx = cons(nlp.model, x)
+  return obj(nlp.model, x) - dot(nlp.y, cx) + (nlp.mu / 2) * dot(cx,cx)
 end
 
 function NLPModels.grad(nlp :: AugLagModel, x :: AbstractVector)
-	increment!(nlp, :neval_grad)
-	cx = cons(nlp.model, x)
-	return grad(nlp.model, x) - jtprod(nlp.model, x, nlp.y) + nlp.mu * jtprod(nlp.model, x, cx)
+  increment!(nlp, :neval_grad)
+  cx = cons(nlp.model, x)
+  return grad(nlp.model, x) - jtprod(nlp.model, x, nlp.y) + nlp.mu * jtprod(nlp.model, x, cx)
 end
 
 function NLPModels.grad!(nlp :: AugLagModel, x :: AbstractVector, g :: AbstractVector)
-	increment!(nlp, :neval_grad)
-	cx = cons(nlp.model, x)
-	g .= grad(nlp.model, x) - jtprod(nlp.model, x, nlp.y) + nlp.mu * jtprod(nlp.model, x, cx)
+  increment!(nlp, :neval_grad)
+  cx = cons(nlp.model, x)
+  g .= grad(nlp.model, x) - jtprod(nlp.model, x, nlp.y) + nlp.mu * jtprod(nlp.model, x, cx)
 end
 
 function NLPModels.hess(nlp :: AugLagModel, x :: AbstractVector; obj_weight :: Float64 = 1.0)
-	increment!(nlp, :neval_hess)
-	cx = cons(nlp.model, x)
-	Jx = jac(nlp.model, x)
-	return obj_weight * (hess(nlp.model, x, obj_weight = obj_weight, y = nlp.mu * cx - nlp.y) + nlp.mu * tril(Jx' * Jx))
+  increment!(nlp, :neval_hess)
+  cx = cons(nlp.model, x)
+  Jx = jac(nlp.model, x)
+  return obj_weight * (hess(nlp.model, x, obj_weight = obj_weight, y = nlp.mu * cx - nlp.y) + nlp.mu * tril(Jx' * Jx))
 end
 
 function NLPModels.hprod(nlp :: AugLagModel, x :: AbstractVector, v :: AbstractVector; obj_weight :: Float64 = 1.0)
-	increment!(nlp, :neval_hprod)
-	cx = cons(nlp.model, x)
-	Jv = jprod(nlp.model, x, v) # J(x)v
-	return hprod(nlp.model, x, v, obj_weight = obj_weight, y = nlp.mu * cx - nlp.y) + nlp.mu * jtprod(nlp.model, x, Jv)
+  increment!(nlp, :neval_hprod)
+  cx = cons(nlp.model, x)
+  Jv = jprod(nlp.model, x, v) # J(x)v
+  return hprod(nlp.model, x, v, obj_weight = obj_weight, y = nlp.mu * cx - nlp.y) + nlp.mu * jtprod(nlp.model, x, Jv)
 end
 
 function NLPModels.hess_op(nlp :: AugLagModel, x :: AbstractVector; obj_weight :: Float64 = 1.0)
-	prod = v -> NLPModels.hprod(nlp, x, v; obj_weight = obj_weight)
-	F = typeof(prod)
-	return LinearOperator{Float64,F,F,F}(nlp.meta.nvar, nlp.meta.nvar, true, true, prod, prod, prod)
+  prod = v -> NLPModels.hprod(nlp, x, v; obj_weight = obj_weight)
+  F = typeof(prod)
+  return LinearOperator{Float64,F,F,F}(nlp.meta.nvar, nlp.meta.nvar, true, true, prod, prod, prod)
 end
 
 function NLPModels.hprod!(nlp :: AugLagModel, x :: AbstractVector, v :: AbstractVector, Hv :: AbstractVector;
-	obj_weight :: Float64 = 1.0)
-	cx = cons(nlp.model, x)
-	Jv = jprod(nlp.model, x, v)
-	Hv .= hprod(nlp.model, x, v, obj_weight = obj_weight, y = nlp.mu * cx - nlp.y) + nlp.mu * jtprod(nlp.model, x, Jv)
+  obj_weight :: Float64 = 1.0)
+  cx = cons(nlp.model, x)
+  Jv = jprod(nlp.model, x, v)
+  Hv .= hprod(nlp.model, x, v, obj_weight = obj_weight, y = nlp.mu * cx - nlp.y) + nlp.mu * jtprod(nlp.model, x, Jv)
 end
 
 function NLPModels.hess_op!(nlp :: AugLagModel, x :: AbstractVector, Hv :: AbstractVector; obj_weight :: Float64 = 1.0)
-	prod = v -> NLPModels.hprod!(nlp, x, v, Hv; obj_weight = obj_weight)
-	F = typeof(prod)
-	return LinearOperator{Float64,F,F,F}(nlp.meta.nvar, nlp.meta.nvar, true, true, prod, prod, prod)
+  prod = v -> NLPModels.hprod!(nlp, x, v, Hv; obj_weight = obj_weight)
+  F = typeof(prod)
+  return LinearOperator{Float64,F,F,F}(nlp.meta.nvar, nlp.meta.nvar, true, true, prod, prod, prod)
 end
 
 function NLPModels.hess_structure(nlp :: AugLagModel)
--- src/al.jl
+++ src/al.jl
@@ -6,89 +6,89 @@ using JSOSolvers, Krylov
 
 """Implementation of a augmented Lagrangian method for:
 
-	min f(x)  s.t.  c(x) = 0, l ≦ x ≦ u"""
+  min f(x)  s.t.  c(x) = 0, l ≦ x ≦ u"""
 
 function al(nlp :: AbstractNLPModel; max_iter :: Int = 1000, max_time :: Real = 30.0)
 
-	x = copy(nlp.meta.x0)
-	gp = zeros(nlp.meta.nvar)
-	cx = cons(nlp, x)
-	gx = grad(nlp, x)
-	Jx = jac(nlp, x)
+  x = copy(nlp.meta.x0)
+  gp = zeros(nlp.meta.nvar)
+  cx = cons(nlp, x)
+  gx = grad(nlp, x)
+  Jx = jac(nlp, x)
 
-	# penalty parameter
-	μ = 10.0
-	# Lagrange multiplier
-	y = cgls(Jx', gx)[1]
-	# tolerance
-	eta = 0.5
+  # penalty parameter
+  μ = 10.0
+  # Lagrange multiplier
+  y = cgls(Jx', gx)[1]
+  # tolerance
+  eta = 0.5
 
-	# create initial subproblem
-	al_nlp = AugLagModel(nlp, y, μ)
+  # create initial subproblem
+  al_nlp = AugLagModel(nlp, y, μ)
 
-	# stationarity measure
-	gL =  grad(nlp, x) - jtprod(nlp, x, y)
-	project_step!(gp, x, -gL, nlp.meta.lvar, nlp.meta.uvar) # Proj(x - gL) - x
-	normgp = norm(gp)
-	normcx = norm(cx)
+  # stationarity measure
+  gL =  grad(nlp, x) - jtprod(nlp, x, y)
+  project_step!(gp, x, -gL, nlp.meta.lvar, nlp.meta.uvar) # Proj(x - gL) - x
+  normgp = norm(gp)
+  normcx = norm(cx)
 
-	iter = 0
-	start_time = time()
-	el_time = 0.0
+  iter = 0
+  start_time = time()
+  el_time = 0.0
 
- 	@info log_header([:iter, :fx, :normgp, :normcx], [Int, Float64, Float64, Float64])
+   @info log_header([:iter, :fx, :normgp, :normcx], [Int, Float64, Float64, Float64])
   @info log_row(Any[iter, obj(nlp, x), normgp, normcx])
 
-	# TODO: Add keyword arguments atol, rtol, max_eval
-	solved = normgp ≤ 1e-5 && normcx ≤ 1e-8
-	tired = iter > max_iter || el_time > max_time
+  # TODO: Add keyword arguments atol, rtol, max_eval
+  solved = normgp ≤ 1e-5 && normcx ≤ 1e-8
+  tired = iter > max_iter || el_time > max_time
 
-	#adaptive tolerance
-	#atol = 0.5
+  #adaptive tolerance
+  #atol = 0.5
 
-	while !(solved || tired)
+  while !(solved || tired)
 
-		# solve subproblem
-		S = with_logger(NullLogger()) do
-			tron(al_nlp, x = x)
-		end
-		x = S.solution
-		cx = cons(nlp, x)
-		normcx = norm(cx)
+    # solve subproblem
+    S = with_logger(NullLogger()) do
+      tron(al_nlp, x = x)
+    end
+    x = S.solution
+    cx = cons(nlp, x)
+    normcx = norm(cx)
 
-		if normcx <= eta
-			al_nlp.y = al_nlp.y - al_nlp.mu * cx
-			eta = eta / (al_nlp.mu)^0.9
-		else
-			μ = 100 * μ
-			al_nlp.mu  = μ
-			eta = 1 / μ^0.1
-		end
+    if normcx <= eta
+      al_nlp.y = al_nlp.y - al_nlp.mu * cx
+      eta = eta / (al_nlp.mu)^0.9
+    else
+      μ = 100 * μ
+      al_nlp.mu  = μ
+      eta = 1 / μ^0.1
+    end
 
-		# stationarity measure
-		gL = grad(nlp, x) - jtprod(nlp, x, al_nlp.y)
-		project_step!(gp, x, -gL, nlp.meta.lvar, nlp.meta.uvar) # Proj(x - gL) - x
-		normgp = norm(gp)
+    # stationarity measure
+    gL = grad(nlp, x) - jtprod(nlp, x, al_nlp.y)
+    project_step!(gp, x, -gL, nlp.meta.lvar, nlp.meta.uvar) # Proj(x - gL) - x
+    normgp = norm(gp)
 
-		iter += 1
-		el_time = time() - start_time
-		solved = normgp ≤ 1e-5 && normcx ≤ 1e-8
-		tired = iter > max_iter || el_time > max_time
+    iter += 1
+    el_time = time() - start_time
+    solved = normgp ≤ 1e-5 && normcx ≤ 1e-8
+    tired = iter > max_iter || el_time > max_time
 
     @info log_row(Any[iter, obj(nlp, x), normgp, normcx])
-	end
+  end
 
-	if solved
-		status = :first_order
-	elseif tired
-		if iter > max_iter
-			status = :max_iter
-		end
-		if el_time > max_time
-			status = :max_time
-		end
-	end
+  if solved
+    status = :first_order
+  elseif tired
+    if iter > max_iter
+      status = :max_iter
+    end
+    if el_time > max_time
+      status = :max_time
+    end
+  end
 
-	return GenericExecutionStats(status, nlp, solution = x, objective = obj(nlp, x), dual_feas = normgp, primal_feas = normcx,
-			iter = iter, elapsed_time = el_time)
+  return GenericExecutionStats(status, nlp, solution = x, objective = obj(nlp, x), dual_feas = normgp, primal_feas = normcx,
+      iter = iter, elapsed_time = el_time)
 end
