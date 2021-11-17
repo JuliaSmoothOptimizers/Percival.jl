1f8b6058004285f95e4ebcbb43c552d2f23749b7
tmigot <tangi.migot@gmail.com>
2021-11-17 06:47:06 +0100

    update make

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,21 @@
+ENV["GKSwstype"] = "100"
 using Documenter, Percival
+
+pages = [
+  "Introduction" => "index.md",
+  "Benchmark" => "benchmark.md",
+  "Reference" => "reference.md",
+]
 
 makedocs(
   sitename = "Percival.jl",
   format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+  modules = [Percival],
+  pages = pages,
 )
 
-deploydocs(repo = "github.com/JuliaSmoothOptimizers/Percival.jl.git", devbranch = "main")
+deploydocs(
+  repo = "github.com/JuliaSmoothOptimizers/Percival.jl.git",
+  push_preview = true,
+  devbranch = "main",
+)
