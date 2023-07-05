17079b8f7452f0da351d717370d8ff84a6527161
tmigot <tangi.migot@gmail.com>
2023-07-05 21:24:39 +0200

    Add allocation tests

---
+++ test/allocs.jl
@@ -0,0 +1,69 @@
+"""
+    @wrappedallocs(expr)
+Given an expression, this macro wraps that expression inside a new function
+which will evaluate that expression and measure the amount of memory allocated
+by the expression. Wrapping the expression in a new function allows for more
+accurate memory allocation detection when using global variables (e.g. when
+at the REPL).
+For example, `@wrappedallocs(x + y)` produces:
+```julia
+function g(x1, x2)
+    @allocated x1 + x2
+end
+g(x, y)
+```
+You can use this macro in a unit test to verify that a function does not
+allocate:
+```
+@test @wrappedallocs(x + y) == 0
+```
+"""
+macro wrappedallocs(expr)
+  argnames = [gensym() for a in expr.args]
+  quote
+    function g($(argnames...))
+      @allocated $(Expr(expr.head, argnames...))
+    end
+    $(Expr(:call, :g, [esc(a) for a in expr.args]...))
+  end
+end
+
+if v"1.7" <= VERSION
+  @testset "Test 0-allocations of NLPModel API for AugLagModel" begin
+    list_of_problems = NLPModelsTest.nlp_problems
+
+    T = Float64
+    for problem in list_of_problems
+      nlp = eval(Symbol(problem))(T)
+      if nlp.meta.ncon > 0
+        μ = one(T)
+        x = nlp.meta.x0
+        fx = obj(nlp, x)
+        y = nlp.meta.y0
+        cx = similar(y)
+        model = Percival.AugLagModel(nlp, y, μ, x, fx, cx)
+
+        test_zero_allocations(model, exclude = [hess])
+      end
+    end
+  end
+
+  @testset "Allocation tests $(model)" for model in setdiff(NLPModelsTest.nlp_problems, ["HS10", "HS13", "LINCON", "LINSV"])
+      nlp = eval(Meta.parse(model))()
+
+      nlp.meta.ncon > 0 || continue
+
+      if !equality_constrained(nlp)
+        nlp = SlackModel(nlp)
+      end
+
+      solver = PercivalSolver(nlp)
+      x = copy(nlp.meta.x0)
+      stats = GenericExecutionStats(nlp)
+      SolverCore.solve!(solver, nlp, stats)
+      reset!(solver)
+      reset!(nlp)
+      al = @wrappedallocs SolverCore.solve!(solver, nlp, stats)
+      @test al == 0
+    end
+end
