c3ae061dcfec992f98b42d6548decf1103cbbc0d
tmigot <tangi.migot@gmail.com>
2023-07-06 19:39:04 -0400

    Add allocation tests (#145)

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,26 +8,7 @@ mutable struct DummyModel{T, S} <: AbstractNLPModel{T, S}
   meta::NLPModelMeta{T, S}
 end
 
-if v"1.7" <= VERSION
-  @testset "Test 0-allocations of NLPModel API for AugLagModel" begin
-    list_of_problems = NLPModelsTest.nlp_problems
-
-    T = Float64
-    for problem in list_of_problems
-      nlp = eval(Symbol(problem))(T)
-      if nlp.meta.ncon > 0
-        μ = one(T)
-        x = nlp.meta.x0
-        fx = obj(nlp, x)
-        y = nlp.meta.y0
-        cx = similar(y)
-        model = Percival.AugLagModel(nlp, y, μ, x, fx, cx)
-
-        test_zero_allocations(model, exclude = [hess])
-      end
-    end
-  end
-end
+include("allocs.jl")
 
 function test()
   nlp = DummyModel(NLPModelMeta(1, minimize = false))
